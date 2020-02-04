ab397253c696ad6fb4fd647de1258a46aef3f6d1
Michael Weiss <mrw@acm.org>
2020-02-04 16:07:34 -0500

    Fix duplicate link problem
    Only add edges in alphabetical order

--- topic_model_explorer.py
+++ topic_model_explorer.py
@@ -30,7 +30,6 @@ def lda_model(url, stopwords, number_of_topics):
 	with st.spinner("Training the topic model ..."):
 		print("*** Training the topic model: {}".format(number_of_topics))
 		lda = tm.fit(corpus, number_of_topics)
-		print("*** Training completed ***")
 		return lda
 
 # move this method to topics
@@ -158,27 +157,30 @@ def keyword_coocurrence_graph(selected_topic, min_edges, cut_off):
 	dtm = document_topics_matrix()
 	top_documents = sort_by_topic(dtm, selected_topic, cut_off)
 	print("*** top documents: {}".format(top_documents))
-	document = "\n".join(corpus.documents['content'][top_documents])
-#	print("*** keyword: {}".format(document))
+	documents = corpus.documents['content'][top_documents]
 	index = {}
 	reverse_index = {}
 	next_index = 0
 	sentence_words = []
-	for sentence in document.split(". "):
-		sentence = re.sub(r'[^A-Za-z0-9]+', ' ', sentence)
-		words = [word for word in sentence.lower().split(" ") 
-			if word not in corpus.stopwords_en]
-		words = set(words)
-		for word in words:
-			if word not in index:
-				index[word] = next_index
-				reverse_index[next_index] = word
-				next_index = next_index + 1
-		sentence_words.append(words)
+	for document in documents:
+		for sentence in document.split(". "):
+			sentence = re.sub(r'[^A-Za-z0-9]+', ' ', sentence)
+			words = [word for word in sentence.lower().split(" ") 
+				if word not in corpus.stopwords_en]
+			words = set(words)
+			for word in words:
+				if word not in index:
+					index[word] = next_index
+					reverse_index[next_index] = word
+					next_index = next_index + 1
+			sentence_words.append(words)
 	edge = np.zeros((len(index), len(index)))
 	for words in sentence_words:
 		for wi, wj in list(itertools.combinations(words, 2)):
-			edge[index[wi], index[wj]] = edge[index[wi], index[wj]] + 1
+			if wi < wj:
+				edge[index[wi], index[wj]] = edge[index[wi], index[wj]] + 1
+			else:
+				edge[index[wj], index[wi]] = edge[index[wj], index[wi]] + 1
 	graph = graphviz.Graph()
 	graph.attr('node', shape='plaintext')
 	nodes = []
@@ -374,8 +376,11 @@ if show_keyword_coocurrences:
 		keywords_min_edges = st.sidebar.slider("Minimum number of edges", 1, 15, value=5)
 		graph, nodes = keyword_coocurrence_graph(keywords_selected_topic, 
 			keywords_min_edges, keywords_cut_off)
-		st.graphviz_chart(graph)
-#		st.write(nodes)
+		if len(nodes) == 0:
+			st.markdown("No graph. Use less restrictive criteria.")
+		else:
+			st.graphviz_chart(graph)
+		st.write(nodes)
 	else:
 		st.markdown("No corpus.")
 
