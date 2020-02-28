3388889f67775b5c9c6cb0f60d07ef0727147043
Michael Weiss <mrw@acm.org>
2020-02-27 21:15:55 -0500

    Highlight color based on weight (initial)

--- topic_model_explorer_stability.py
+++ topic_model_explorer_stability.py
@@ -35,7 +35,7 @@ def lda_model_runs(url, stopwords, number_of_topics, n=4):
 
 def topic_alignment(n):
 	lda_models = lda_model_runs(url, stopwords, number_of_topics, n=n)
-	topic_df = pd.DataFrame([[" ".join([tw[0] for tw in lda.lda.show_topic(t, 10)]) for lda in lda_models]
+	topics = pd.DataFrame([[" ".join([tw[0] for tw in lda.lda.show_topic(t, 10)]) for lda in lda_models]
 		for t in range(number_of_topics)])
 	diff = [lda_models[0].difference(lda_models[i]) for i in range(1, n)]
 	matches = pd.DataFrame()
@@ -43,7 +43,7 @@ def topic_alignment(n):
 	for i in range(1, n):
 		_, cols = linear_sum_assignment(diff[i-1])
 		matches[i] = cols
-	return topic_df, matches, lda_models, diff
+	return topics, matches, lda_models, diff
 
 def highlight_topic(x, topic, matches, color="lightgreen"):
 	color = "background-color: %s" % (color)
@@ -53,13 +53,48 @@ def highlight_topic(x, topic, matches, color="lightgreen"):
 	return df
 
 def topic_runs(lda_models, topic, matches):
-	df = pd.DataFrame()
+	keywords = pd.DataFrame()
+	weights = pd.DataFrame()
 	for run in range(len(lda_models)):
-		df[run] = [tw[0] for tw 
+		keywords[run] = [tw[0] for tw
 			in lda_models[run].lda.show_topic(matches[run][topic], 10)]
+		weights[run] = [tw[1] for tw
+			in lda_models[run].lda.show_topic(matches[run][topic], 10)]
+	return keywords, weights
+
+# todo: once we pass weights, use the relative weights to assign colors
+# relative weight = weight / lowest weight in top 10
+# for all keywords that are repeated across topics, color them blue if all >= 2, 
+# green if some >= 2, and blue if all < 2
+def highlight_repeated_keywords(keywords, weights):
+	df = pd.DataFrame('', keywords.index, keywords.columns)
+	num_runs, num_words = len(keywords.columns), len(keywords.index)
+	# extract array from data frame
+	# we transpose the array so that each row represents one run
+	keywords = keywords.values.T
+	weights = weights.values.T
+	repeated_keywords = []
+	for keyword in keywords[0]:
+		i = 0
+		for run in range(1, num_runs):
+			if keyword in keywords[run]:
+				i = i + 1
+		# print("keyword {} occurs {} times".format(keyword, i))
+		if i == num_runs - 1:
+			repeated_keywords.append(keyword)
+	print("Repeated keywords: {}".format(repeated_keywords))
+	for j in range(num_runs):
+		for i in range(num_words):
+			if keywords[j,i] in repeated_keywords:
+				print("weights: {} / {}".format(weights[j,i], weights[j,num_words-1]))
+				ratio = weights[j,i]/weights[j,num_words-1]
+				if ratio >= 2.0:
+					df[j].loc[i] = "background-color: yellow"
+				else:
+					df[j].loc[i] = "background-color: lightblue"
 	return df
 
-def highlight_repeated_keywords(x, color="yellow"):
+def highlight_repeated_keywords_v1(x, color="yellow"):
 	color = "background-color: %s" % (color)
 	df = pd.DataFrame('', x.index, x.columns)
 	# extract array from data frame
@@ -132,13 +167,16 @@ if show_runs:
 	if url is None:
 		st.markdown("No corpus")
 	elif show_runs_all_topics:
-		topic_df, matches, _, _ = topic_alignment(5)
-		st.table(topic_df.style
+		topics, matches, lda_models, diff = topic_alignment(5)
+		st.table(topics.style
 			.apply(highlight_topic, topic=topic_to_highlight, matches=matches, axis=None))
 	else:
-		topic_df, matches, lda_models, diff = topic_alignment(5)
-		topic_runs_df = topic_runs(lda_models, topic=topic_to_highlight, matches=matches)
-		st.table(topic_runs_df.style
-			.apply(highlight_repeated_keywords, axis=None))
+		# todo: topic_alignment to return weights as well
+		# then pass weights as argument to highlight_repeated_keywords
+		topics, matches, lda_models, diff = topic_alignment(5)
+		keywords, weights = topic_runs(lda_models, topic=topic_to_highlight, matches=matches)
+		st.table(keywords.style
+			.apply(highlight_repeated_keywords, weights=weights, axis=None))
+		st.table(weights)
 
 
