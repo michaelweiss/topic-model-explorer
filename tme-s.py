be226b34453cf702114cdfc80158f7dd1044e800
Michael Weiss <mrw@acm.org>
2020-03-29 19:15:38 -0400

    Refactored alignment code into its own class
    Creating the class TopicAlignment avoided the caching issue. Once
    computed, the topic model runs are no longer (arbitrarily) recomputed.
    This is achieved by not storing the computed LDA models in the
    TopicAlignment instance. Somehow Streamlit thought that these LDA
    models have been modified outside of lda_model_runs().

--- tme-s.py
+++ tme-s.py
@@ -5,56 +5,32 @@ from scipy.optimize import linear_sum_assignment
 import time
 
 from topics import TopicModel
+from topics import TopicAlignment
 
 # model
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def load_corpus(url):
+def load_corpus(url, stopwords):
 	st.markdown("Cache miss: load_corpus")
-	return tm.load_corpus(url)
-
-def update_stopwords(corpus, stopwords):
-	if corpus is not None:
-		corpus.update_stopwords(stopwords)
+	return tm.load_corpus(url, stopwords)
 
 @st.cache(suppress_st_warning=True)
-def lda_model_runs(corpus, number_of_topics, number_of_chunks, number_of_runs):
+def find_topic_alignment(corpus, number_of_topics, number_of_chunks, number_of_runs):
 	status = st.markdown("Fitting topic models:")
 	progress_bar = st.progress(0)
-	lda_models = []
-	for run in range(number_of_runs):
-		lda_models.append(tm.fit(corpus, number_of_topics, number_of_chunks=number_of_chunks))
-		# lda_models.append([run, number_of_topics, number_of_chunks])
-		# time.sleep(1)
+	def progress_update(run):
 		progress_bar.progress(int(100 * (run + 1)/number_of_runs))
+	alignment = TopicAlignment(tm, corpus, number_of_topics, number_of_chunks, number_of_runs)
+	alignment.fit(progress_update)
 	hide_status_indicators(status, progress_bar)
-	return lda_models
+	return alignment
 
 # model helpers
-
-def topic_alignment(lda_models, number_of_topics):
-	# extract the topic words for each topic in all topic models
-	topics = pd.DataFrame([[" ".join([tw[0] for tw in lda.lda.show_topic(t, 10)]) 
-		for lda in lda_models] for t in range(number_of_topics)])
-	# compute the average Jaccard distance between the topic models
-	diffs = [lda_models[0].difference(lda_models[i]) 
-		for i in range(1, len(lda_models))]
-	matches = pd.DataFrame()
-	# first column are the topics of the first topic model
-	matches[0] = range(number_of_topics)
-	# try to fit topics between the first and each of the remaining topic models
-	# the Hungarian assignment method uses the degree of match between topics (diffs)
-	# and minimizes the total misalignment between topics
-	for i in range(1, len(lda_models)):
-		_, cols = linear_sum_assignment(diffs[i-1])
-		# each column contains the topics that match the topics of the first topic
-		matches[i] = cols
-	return topics, matches, diffs
 
 # view
 
 def show_documents(corpus):
-	st.header("Corpus")
+	st.header("Documents")
 	if corpus is not None:
 		check_for_name_content_columns(corpus.documents)
 		st.dataframe(corpus.documents)
@@ -66,15 +42,19 @@ def show_topic_model_runs(corpus, number_of_topics, number_of_chunks, number_of_
 	if corpus is None:
 		st.markdown("Please upload a corpus first")
 	else:
-		selected_topic = st.sidebar.selectbox("Highlight topic", range(number_of_topics), 0)
-		lda_models = lda_model_runs(corpus, number_of_topics, number_of_chunks, number_of_runs)
-		# topics, matches, diffs = topic_alignment(lda_models, number_of_topics)
+		selected_topic = st.sidebar.selectbox("Select topic to highlight", range(number_of_topics), 0)
+		alignment = find_topic_alignment(corpus, number_of_topics, number_of_chunks, number_of_runs)
 		if st.sidebar.checkbox("Show all topics", value=False):
-			st.write(lda_models)
-		# 	st.table(topics.style
-		# 		.apply(highlight_topic, topic=selected_topic, matches=matches, axis=None))
+			"""
+			Highlighting shows which topics correspond across the different runs.
+			The alignment is calculated based on minimizing the sum of the differences between topics.
+			"""
+			st.table(alignment.topics.style
+				.apply(highlight_topic, topic=selected_topic, matches=alignment.matches, axis=None))
 		else:
-		 	st.markdown("Show one topic only")
+			"""
+			Show a single topic only.
+			"""
 
 # view helpers
 
@@ -101,12 +81,10 @@ def hide_status_indicators(*indicators):
 def app(tm):
 	st.sidebar.title("Topic Model Explorer")
 	url = st.sidebar.file_uploader("Corpus", type="csv")
-	corpus = load_corpus(url)
+	stopwords = st.sidebar.text_area("Stopwords (one per line)")
+	corpus = load_corpus(url, stopwords)
 	if st.sidebar.checkbox("Show documents"):
 		show_documents(corpus)
-	stopwords = st.sidebar.text_area("Stopwords (one per line)")
-	if st.sidebar.button("Update stopwords"):
-		update_stopwords(corpus, stopwords)
 	number_of_topics = st.sidebar.slider("Number of topics", 1, 50, 10)
 	number_of_chunks = st.sidebar.slider("Number of chunks", 1, 100, 100)
 	number_of_runs = st.sidebar.slider("Number of runs", 1, 10, 4)
--- topics.py
+++ topics.py
@@ -11,6 +11,7 @@ from gensim.corpora import Dictionary
 
 import pandas as pd
 import numpy as np
+from scipy.optimize import linear_sum_assignment
 import math
 
 from io import StringIO
@@ -20,12 +21,12 @@ class TopicModel:
 	def gensim_version(self):
 		return gs.__version__
 
-	def load_corpus(self, url):
+	def load_corpus(self, url, stopwords):
 		if url is not None:
 			url.seek(0)	 # move read head back to the start (StringIO behaves like a file)
 			documents = pd.read_csv(url)
 			corpus = Corpus(documents)
-			corpus.preprocess()
+			corpus.preprocess(stopwords)
 			return corpus
 		else:
 			return None
@@ -68,20 +69,18 @@ class TopicModel:
 	# def topics(self, number_of_words):
 	# 	return self.read_topics(self.topics_to_csv(number_of_words))
 
-# TODO: move logic for running multiple topic models from topic-model-explorer
-class TopicModelRuns:
-	def __init__(self, topic_model, url, stopwords, number_of_chunks, number_of_topics, n=5):
-		self.topic_model = topic_model
-		self.url = url
-		self.stopwords = stopwords
-		self.number_of_chunks = number_of_chunks
-		self.number_of_topics = number_of_topics
-		self.n = n
-		# run_topic_models()
-
 class LDA:
 	def __init__(self, lda):
 		self.lda = lda
+
+	def corpus(self):
+		return lda.corpus  # bow representation, our corpus.bow()
+
+	def number_of_topics(self):
+		return lda.num_topics
+
+	def chunksize(self):
+		return lda.chunksize
 
 	def show_topics(self, number_of_topics, number_of_words):
 		return self.lda.show_topics(num_topics=number_of_topics, 
@@ -104,10 +103,55 @@ class LDA:
 		diff, _ = self.lda.diff(other.lda, distance='jaccard', num_words=k)
 		return diff
 
+class TopicAlignment:
+	def __init__(self, topic_model, corpus, number_of_topics, number_of_chunks, number_of_runs):
+		self.topic_model = topic_model
+		self.corpus = corpus
+		self.number_of_topics = number_of_topics
+		self.number_of_chunks = number_of_chunks
+		self.number_of_runs = number_of_runs
+
+	def fit(self, progress_update):
+		lda_models = self.lda_model_runs(progress_update)
+		self.topics = self.topics(lda_models)
+		self.matches = self.matches(lda_models)
+
+	# create a group of topic models with the same number of topics
+	def lda_model_runs(self, progress_update):
+		lda_models = []
+		for run in range(self.number_of_runs):
+			lda_models.append(self.topic_model.fit(self.corpus, self.number_of_topics, 
+				number_of_chunks=self.number_of_chunks))
+			progress_update(run)
+		return lda_models
+
+	# extract the topic words for each topic in all topic models
+	def topics(self, lda_models):
+		return pd.DataFrame([[" ".join([tw[0] for tw in lda_model.lda.show_topic(t, 10)]) 
+			for lda_model in lda_models] for t in range(self.number_of_topics)])
+
+	# compute the average Jaccard distance between the topic models
+	def differences(self, lda_models):
+		return [lda_models[0].difference(lda_models[i]) 
+			for i in range(1, len(lda_models))]
+
+	# fit topics between the first and each of the remaining topic models using
+	# the Hungarian linear assignment method
+	def matches(self, lda_models):
+		diffs = self.differences(lda_models)
+		matches = pd.DataFrame()
+		# first column are the topics of the first topic model
+		matches[0] = range(self.number_of_topics)
+		# minimize the total misalignment between topics
+		for i in range(1, len(lda_models)):
+			_, cols = linear_sum_assignment(diffs[i-1])
+			# each column contains the topics that align with the topics of the first topic
+			matches[i] = cols
+		return matches
+
 class Corpus:
 	def __init__(self, documents):
 		self.documents = self.to_ascii(documents)
-		self.initialize_stopwords()
 
 	def to_ascii(self, documents):
 		# replace non-ascii symbols left by text processing software
@@ -115,27 +159,18 @@ class Corpus:
 			for document in documents['content']]
 		return documents
 
-	def preprocess(self):
+	def preprocess(self, user_defined_stopwords):
+		self.stopwords_en = self.read_stopwords("stopwords-en.txt")
+		self.user_defined_stopwords = user_defined_stopwords.split('\n')
+		self.stopwords = self.stopwords_en + self.user_defined_stopwords
 		self.tokens = [[word for word in sub(r'[^A-Za-z0-9]+', ' ', document).lower().split() 
 				if word not in self.stopwords] 
 			for document in self.documents['content']]
 		self.dictionary = Dictionary(self.tokens)
 
-	def initialize_stopwords(self):
-		self.read_stopwords("stopwords-en.txt")
-
 	def read_stopwords(self, file):
 		file = open(file, 'r')
-		self.stopwords_en = file.read().split('\n')
-		self.stopwords = self.stopwords_en
-
-	def update_stopwords(self, stopwords):
-		print("*** Update stopwords: {}".format(stopwords.split('\n')))
-		new_stopwords = stopwords.split('\n')
-		self.initialize_stopwords()
-		self.stopwords = self.stopwords + new_stopwords
-		# update tokens after adding stopwords
-		self.preprocess()
+		return file.read().split('\n')
 
 	def bow(self):
 		return [self.dictionary.doc2bow(doc) for doc in self.tokens]
