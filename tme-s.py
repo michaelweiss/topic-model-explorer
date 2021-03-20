1be8cd7916a96e70191575a2e24855313d3c21b1
michaelweiss <weiss@sce.carleton.ca>
2021-03-20 14:01:58 -0400

    show keywords for each topic / 2

--- tme-s.py
+++ tme-s.py
@@ -84,7 +84,7 @@ def show_topic_model_runs(corpus, number_of_topics, number_of_chunks, number_of_
 				document_bow = corpus.get_document_bow(new_document)
 				topics = alignment.lda_models[0].get_document_topics(document_bow)
 				for t, w in topics:
-					keywords = ", ".join([tw[0] for tw in alignment.lda_model[0].lda.show_topic(t, 3)])
+					keywords = ", ".join([tw[0] for tw in alignment.lda_models[0].lda.show_topic(t, 3)])
 					st.write("Topic {} ({}) with weight {}".format(t, keywords, w))
 		else:
 			"""
