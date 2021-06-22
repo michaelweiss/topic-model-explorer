95a5f02412950772e071ad875b8b58bc0db27f2f
michaelweiss <weiss@sce.carleton.ca>
2021-06-22 14:07:54 -0400

    added headers for model and view

--- tme.py
+++ tme.py
@@ -6,29 +6,51 @@ import numpy as np
 
 from topics import TopicModel
 
+# model
+
 @st.cache(allow_output_mutation=True)
 def load_corpus(url, stopwords, multiwords):
 	return tm.load_corpus(url, stopwords, multiwords)
 
-st.sidebar.title("Topic Model Explorer")
+# view
+
+def show_documents(corpus):
+	st.header("Documents")
+	if corpus is not None:
+		check_for_name_content_columns(corpus.documents)
+		st.dataframe(corpus.documents, height=150)
+		# download_link_from_csv("\n".join(corpus.stopwords), "stopwords.txt",
+		# 	"Download stopwords")
+	else:
+		st.markdown("Please upload a corpus.")
+
+# view helpers
+
+def check_for_name_content_columns(documents):
+	if 'name' not in documents or 'content' not in documents:
+		st.markdown('''
+		The corpus must have a *name* and a *content* column.
+		''')
+
+# controller
+
 tm = TopicModel()
 
+st.sidebar.title("Topic Model Explorer")
 st.sidebar.write("Using streamlist {} and gensim {}".format(st.__version__, tm.gensim_version()))
 
 url = st.sidebar.file_uploader("Corpus", type="csv")
-show_documents = st.sidebar.checkbox("Show documents", value=True)
+stopwords = ""
+multiwords = ""
+corpus = load_corpus(url, stopwords, multiwords)
 
-if show_documents:
-	st.header("Corpus")
-	if url is not None:
-		corpus = load_corpus(url, "", "")
-		if(corpus is None):
-			st.markdown('''
-		__Error:__ The corpus must have a *name* and a *content* column.
-			''')
-		else:
-			st.dataframe(corpus.documents, height=150)
-			# download_link_from_csv("\n".join(corpus.stopwords), "stopwords.txt",
-			# 	"Download stopwords")
-	else:
-		st.markdown("Please upload a corpus.")
+if st.sidebar.checkbox("Show documents"):
+	show_documents(corpus)
+
+# stopwords = st.sidebar.text_area("Stopwords (one per line)")
+# update_stopwords = st.sidebar.button("Update stopwords")
+
+# if update_stopwords:
+# 	if url is not None:
+# 		corpus = load_corpus(url, "", "")
+# 		corpus.update_stopwords(stopwords)
