263c4d8ab4ac64c38bfd7ba21acbc74b5c4d45a4
michaelweiss <weiss@sce.carleton.ca>
2021-06-23 23:05:13 -0400

    topic co-occurrence

--- tme.py
+++ tme.py
@@ -3,7 +3,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import math
+import itertools
 import base64
+import graphviz as graphviz
 
 from topics import TopicModel, LDA
 
@@ -23,6 +26,32 @@ def topics(model):
 
 def document_topic_matrix(model, corpus):
 	return model.document_topic_matrix(corpus)
+
+# sum document frequencies for each topic and normalize
+# thus, the column tallies add up to 1
+def tally_columns(dtm, number_of_topics):
+	return [sum([row[k] for row in dtm])/len(dtm) for k in range(number_of_topics)]
+
+def topic_coocurrence_graph(model, corpus, number_of_topics, min_weight, min_edges):
+	dtm = document_topic_matrix(model, corpus).to_numpy()
+	keywords = ["\n".join([tw[0] for tw in model.lda.show_topic(t, 3)])
+		for t in range(number_of_topics)]
+	graph = graphviz.Graph()
+	graph.attr('node', shape='circle', fixedsize='true')
+	total_topic_weights = tally_columns(dtm, number_of_topics)
+	for i in range(number_of_topics):
+		graph.node(str(i), width=str(4*math.sqrt(total_topic_weights[i])), label=keywords[i])
+	edge = np.zeros((number_of_topics, number_of_topics))
+	for topic_weights in dtm:
+		topics = [k for k in range(number_of_topics) if topic_weights[k] >= min_weight]
+		for i, j in list(itertools.combinations(topics, 2)):
+			edge[i, j] = edge[i, j] + 1
+	for i in range(number_of_topics):
+		for j in range(number_of_topics):
+			if edge[i, j] >= min_edges:
+				graph.edge(str(i), str(j), 
+					penwidth="{}".format(edge[i, j]))
+	return graph
 
 # view
 
@@ -60,6 +89,24 @@ def show_document_topic_matrix(corpus, number_of_topics, number_of_chunks=100):
 		st.dataframe(dtm_df, height=150)
 		download_link(dtm_df, "document-topic-matrix-{}.csv".format(number_of_topics),
 			"Download document topic matrix")
+
+def show_topic_co_occurrences(corpus, number_of_topics, number_of_chunks=100):
+	st.header("Topic co-occurrences")
+	if corpus is None:
+		st.markdown("Please upload a corpus first")
+	else:
+		min_weight = st.sidebar.slider("Minimum weight", 0.0, 0.5, value=0.1)
+		min_edges = st.sidebar.slider("Minimum number of edges", 1, 10, value=1)
+		st.markdown('''
+			We consider topics to co-occur in the same document if the weight of both 
+			topics for that document are greater than *minimum weight*. The thickness of
+			an edge in the co-occurrance graph indicates how often two topics co-occur
+			in a document (at least *minimum edges* times). Each node represents a 
+			topic. Node size reflects the total weight of the topic.
+		''')
+		graph = topic_coocurrence_graph(topic_model(corpus, number_of_topics, number_of_chunks), 
+			corpus, number_of_topics, min_weight, min_edges)
+		st.graphviz_chart(graph)
 
 # view helpers
 
@@ -103,3 +150,6 @@ if st.sidebar.checkbox("Show topics", value=False):
 
 if st.sidebar.checkbox("Show document topic matrix", value=False):
 	show_document_topic_matrix(corpus, number_of_topics, number_of_chunks)
+
+if st.sidebar.checkbox("Show topic co-occurrences", value=False):
+	show_topic_co_occurrences(corpus, number_of_topics, number_of_chunks)
